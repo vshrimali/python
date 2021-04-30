fa6b66eb485215b2712a8ca859eb096ba1fc798d
vshrimali <vihanshrimali1@gmail.com>
2021-04-30 23:45:34 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+b= 54654.44
+c = 4445654
+d = None
+e = True
+# printing the type of variable
+print (type(e))
+print("the value of 54654.44 + 4445654 is",c+b)
+bool1 = True
+bool2 = False
+print (type (b ))
+a = 458
+b = 34
+print (a)
+print (a==b)
